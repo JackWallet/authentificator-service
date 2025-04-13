1a2d1ac5739fbf1068bc03b8d5625daa30952047
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-13 17:44:18 +0300

    init api_key

---
+++ auth_service/domain/api_key.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+from datetime import datetime
+from enum import StrEnum
+
+
+class AccessLevelEnum(StrEnum):
+    READ = "read"
+    WRITE = "write"
+
+
+@dataclass
+class APIKey:
+    key: str
+    access_level: AccessLevelEnum
+    created_at: datetime
