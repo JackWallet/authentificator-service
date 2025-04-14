5d50238f8c14dd2f7deaa1071a33d432494b3b26
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-14 23:34:01 +0300

    api key repository protocol

--- auth_service/application/abstract/database/repositories/api_key_repository.py
+++ auth_service/application/abstract/database/repositories/api_key_repository.py
@@ -2,4 +2,11 @@ from abc import abstractmethod
 from typing import Protocol
 
 
-class ApiKeyRepository(Protocol): ...
+class ApiKeyRepository(Protocol):
+    @abstractmethod
+    def is_allowed_to_read(self, key_id: str) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def is_allower_to_write(self, key_id: str) -> bool:
+        raise NotImplementedError
