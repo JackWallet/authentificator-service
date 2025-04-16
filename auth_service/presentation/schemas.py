543d94abe9716dbcd64f980badbbd3c8f6b25606
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-16 22:44:38 +0300

    Split api keys in two

--- auth_service/presentation/schemas.py
+++ auth_service/presentation/schemas.py
@@ -1,10 +1,19 @@
 from pydantic import BaseModel, Field
 
 
-class APIKeySchema(BaseModel):
+class APIReaderKeySchema(BaseModel):
     api_key: str = Field(
         max_length=256,
         min_length=256,
         description="Only 256 characters are allowed for the api key",
         alias="X-AUTH_READER_API_KEY",
+    )
+
+
+class APIWriterKeySchema(BaseModel):
+    api_key: str = Field(
+        max_length=256,
+        min_length=256,
+        description="Only 256 characters are allowed for the api key",
+        alias="X-AUTH_WRITER_API_KEY",
     )
