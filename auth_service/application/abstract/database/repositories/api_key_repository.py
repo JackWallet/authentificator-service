78bd55390925116bee7986fe2d1e000c535e2578
InWamos <mykyta.pidgornyi@gmail.com>
2025-04-16 22:54:36 +0300

    add api key writer repository interface

--- auth_service/application/abstract/database/repositories/api_key_repository.py
+++ auth_service/application/abstract/database/repositories/api_key_repository.py
@@ -1,6 +1,8 @@
 from abc import abstractmethod
 from typing import Protocol
 
+from auth_service.domain.api_key import APIKey
+
 
 class ApiKeyReaderRepository(Protocol):
     @abstractmethod
@@ -9,4 +11,14 @@ class ApiKeyReaderRepository(Protocol):
 
     @abstractmethod
     def is_allower_to_write(self, key_id: str) -> bool:
+        raise NotImplementedError
+
+
+class ApiKeyWriterRepository(Protocol):
+    @abstractmethod
+    def add_api_key(self, api_key: APIKey) -> None:
+        raise NotImplementedError
+
+    @abstractmethod
+    def revoke_api_key(self, key: str) -> None:
         raise NotImplementedError
